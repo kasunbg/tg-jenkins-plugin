a3e8f723011873e99b60771bb33794a4c842d235
Sameera Wickramasekara <sameeraw@wso2.com>
2018-11-08 09:00:38 +0530

    Initial commit with plugin structure

---
+++ src/main/java/org/wso2/carbon/testgrid/TGJobProperty.java
@@ -0,0 +1,53 @@
+/*
+* Copyright (c) 2017, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+*
+* WSO2 Inc. licenses this file to you under the Apache License,
+* Version 2.0 (the "License"); you may not use this file except
+* in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing,
+* software distributed under the License is distributed on an
+* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+* KIND, either express or implied. See the License for the
+* specific language governing permissions and limitations
+* under the License.
+*/
+package org.wso2.carbon.testgrid;
+
+import hudson.Extension;
+import hudson.model.Descriptor;
+import hudson.model.Job;
+import hudson.model.JobProperty;
+import hudson.model.JobPropertyDescriptor;
+import net.sf.json.JSONObject;
+import org.kohsuke.stapler.StaplerRequest;
+
+import javax.annotation.Nonnull;
+
+public final class TGJobProperty extends JobProperty<Job<?,?>>{
+
+
+    @Extension
+    public static class PropertyImpl extends JobPropertyDescriptor{
+
+        @Override
+        public boolean isApplicable(Class<? extends Job> jobType) {
+            //TODO applicable only in pipeline projects
+            return true;
+        }
+
+        @Override
+        public JobProperty<?> newInstance(StaplerRequest req, JSONObject formData) throws FormException {
+            return super.newInstance(req, formData);
+        }
+
+        @Nonnull
+        @Override
+        public String getDisplayName() {
+            return "TestGrid";
+        }
+    }
+}
